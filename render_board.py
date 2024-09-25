bde123a6afa1b677994d3c076264449294fcc4b3
justinjest <jest.cayaban@gmail.com>
2024-09-25 19:49:07 +0900

    added basic chess board rendering ability

--- render_board.py
+++ render_board.py
@@ -1,7 +1,7 @@
 def create_board():
     board = {}
-    for x in range (0,8):
-        for y in range (0, 8):
+    for y in range (7, -1, -1):
+        for x in range (0, 8):
             board[f"{name_column(x)}{y + 1}"] = ""
     return (board)
 
@@ -21,10 +21,27 @@ def name_column(col_num):
     return replacement[col_num]
 
 def draw_board(board):
-    for x in range(0, 8):
-        for y in range (0, 8):
-            print ("""|   |
-                   |    |
-                   _____""")
-            
-create_board()
+    spacer = " _"
+    for i in range(0, 8):
+        print (spacer, end = "")
+    print ("")
+    for y in range(7, -1, -1):
+        for x in range (0, 8):
+            draw_square(x, y, board)
+        print ("")
+
+def draw_square(x, y, board):
+    piece = "_"
+    pieces = ["p",
+              "b",
+              "n",
+              "r",
+              "q",
+              "k"]
+    tmp = board[f"{name_column(x)}{y+1}"]
+    if tmp in pieces:
+        piece = tmp
+    if x < 7:
+        print (f"|{piece}", end = "")
+    if x == 7:
+        print(f"|{piece}|", end = "")
